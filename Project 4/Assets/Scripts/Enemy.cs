05c172f2668d4d4bd9bf84b66e1b89bb0e85e6ce
CarterHanes <25hanec@elida.k12.oh.us>
2022-10-04 12:27:35 -0400

    continued 4.2

--- Project 4/Assets/Scripts/Enemy.cs
+++ Project 4/Assets/Scripts/Enemy.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{ //HEY, YOURE ON 4.2 STINKY
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
         
     }
 }
