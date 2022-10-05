ac7c404fcc7ccdcd3a3bb417a30cf018d370ae7e
CarterHanes <25hanec@elida.k12.oh.us>
2022-10-05 12:26:58 -0400

    4.3 start

--- Project 4/Assets/Scripts/PlayerController.cs
+++ Project 4/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,35 @@ public class PlayerController : MonoBehaviour
 {   public float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("FocalPoint");
+        if (other.CompareTag("Powerup")) 
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
         
+        }
     }
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<enemyRigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("Collided with" + collision.gameObject.name
+                + "with powerup set to" + hasPowerup);
+
+        }
+    }
+        void Start() 
+        { 
+            playerRb = GetComponent<Rigidbody>();
+            focalPoint = GameObject.Find("FocalPoint");
+        
+        }
 
     // Update is called once per frame
     void Update()
