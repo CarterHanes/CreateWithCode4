7558659fc6f5effb2c8f52d459d3b2cdcaae9b57
CarterHanes <25hanec@elida.k12.oh.us>
2022-10-06 12:27:28 -0400

    4.3.6

--- Project 4/Assets/Scripts/PlayerController.cs
+++ Project 4/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public bool hasPowerup;
-    private float powerupStrength = 15.0f;
+    public float powerupStrength = 15.0f;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
@@ -15,17 +15,25 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         
         }
     }
+    IEnumerator PowerupCountdownRoutine() 
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+    }
+
     private void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
-            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<enemyRigidbody>();
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
             Debug.Log("Collided with" + collision.gameObject.name
                 + "with powerup set to" + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
 
         }
     }
